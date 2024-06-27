1061b2e18f73329606c304d1053b0f06d3b42065
Agustin Suarez <titoagustinsuarez6@gmail.com>
2024-06-27 15:35:50 -0300

    Server.js reconoce /public

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.set("public", path.join(__dirname, "public"));
+
 app.use("/", express.static(path.join(__dirname, "/views")));
 
 app.set("views", path.join(__dirname, "views")); // Ruta a la carpeta "views"
