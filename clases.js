df3cbc4e69432f8dd004b3e6cbd7afa6996280eb
AlejoMontuori <Alejomontuori12@gmail.com>
2024-06-28 20:58:12 -0300

    Clases realizadas

--- clases.js
+++ clases.js
@@ -1,5 +1,6 @@
 class Pieza {
-    constructor(nroR, nomP, medidaP, materialOb, fechaAd, formaAdq, añoPi, estadoPi, cantidad, Obse) {
+    constructor(idPie, nroR, nomP, medidaP, materialOb, fechaAd, formaAdq, añoPi, estadoPi, cantidad, Obse) {
+        this.idPieza = idPie;
         this.NumeroRegistro = nroR;
         this.NombrePieza = nomP;
         this.MedidaPieza = medidaP;
@@ -16,6 +17,7 @@ class Pieza {
     static fromJSON(json) {
         if (json.class == "Pieza") {
             let nuevaPieza = new Pieza();
+            nuevaPieza.idPieza = json.idPieza;
             nuevaPieza.NumeroRegistro = json.NumeroRegistro;
             nuevaPieza.NombrePieza = json.NombrePieza;
             nuevaPieza.MedidaPieza = json.MedidaPieza;
@@ -32,6 +34,160 @@ class Pieza {
     }
 }
 
+
+
+class Prestamo{
+
+   constructor(idPres, eventoPre, obsePre, fechaPre, idPieza){
+        this.idPrestamo = idPres;
+        this.eventoPrestamo = eventoPre;
+        this.observacionPrestamo = obsePre;
+        this.fechaPrestamo = fechaPre;
+        this.idPieza = idPieza; 
+        this.class = "Prestamo";
+
+   }
+
+   static fromJSON(json){
+
+    if(json.class == "Prestamo"){
+
+        let nuevoPrestamo = new Prestamo();
+
+        nuevoPrestamo.idPrestamo = json.idPrestamo;
+        nuevoPrestamo.eventoPrestamo = json.eventoPrestamo;
+        nuevoPrestamo.observacionPrestamo = json.observacionPrestamo;
+        nuevoPrestamo.fechaPrestamo = json.fechaPrestamo;
+        nuevoPrestamo.idPieza = json.idPieza;
+        nuevoPrestamo.class = json.class;
+        return nuevoPrestamo;
+
+    }
+
+   }
+
+}
+
+
+class Prestamoestado{
+    
+    constructor(idPrEs, band, estadoPi){
+
+        this.idPrestamoEstado = idPrEs;
+        this.bandera = band;
+        this.estadoPieza = estadoPi;
+        this.class = "Prestamoestado"
+
+    }
+    
+    static fromJSON(json){
+
+        if(json.class == "Prestamoestado"){
+
+            let NuevoPresEstado = new Prestamoestado();
+
+            NuevoPresEstado.idPrestamoEstado = json.idPrestamoEstado;
+            NuevoPresEstado.bandera = json.bandera;
+            NuevoPresEstado.estadoPieza = json.estadoPieza;
+            return NuevoPresEstado; 
+
+        }
+
+    }
+}
+
+
+
+
+
+class Taxidermia {
+
+    constructor(idTax, fechaM, obseTax, idPieza){
+
+        this.idTaxidermia = idTax;
+        this.fechaMantenimiento = fechaM;
+        this.observacionTaxidermia = obseTax;
+        this.idPieza = idPieza;
+        this.class == "Taxidermia";
+    }
+
+    static fromJSON(json){
+
+        if(json.class == "Taxidermia"){
+
+        let nuevoTaxi = new Taxidermia();
+
+        nuevoTaxi.idTaxidermia = json.idTaxidermia;
+        nuevoTaxi.fechaMantenimiento = json.fechaMantenimiento;
+        nuevoTaxi.observacionTaxidermia = json.observacionTaxidermia;
+        nuevoTaxi.idPieza = json.idPieza;
+        return nuevoTaxi;
+
+        }
+
+    }
+
+}
+
+
+
+
+
+class Piezaestado{
+
+    constructor(idPEs, ban){
+        this.idPiezaEstado = idPEs;
+        this.bandera = ban;
+        this.class == "Piezaestado" 
+
+    }
+
+    static fromJSON(json){
+
+        
+        if(json.class == "Piezaestado"){
+        let NuevoPiezaEstado = new Piezaestado();
+           
+            NuevoPiezaEstado.idPiezaEstado = json.idPiezaEstado;
+            NuevoPiezaEstado.bandera = json.Bandera; 
+            return NuevoPiezaEstado;
+        } 
+            
+    }
+ }
+
+
+
+
+ class EstadiPieza{
+    constructor(idEP, fechaEP, idPieza, idPE){
+        this.idEstadoPieza = idEP;
+        this.fechaEstado = fechaEP;
+        this.idPieza = idPieza;
+        this.idPiezaEstado = idPE;
+        this.class == "estadiPieza";
+    }
+
+    static fromJSON(json){
+
+        if(json.class == "estadiPieza"){
+
+            let NuevoEstadiPieza = new estadiPieza();
+            
+            NuevoEstadiPieza.idEstadoPieza = json.idEstadoPieza;
+            NuevoEstadiPieza.fechaEstado = json.fechaEstado;
+            NuevoEstadiPieza.idPieza = json.idPieza;
+            NuevoEstadiPieza.idPiezaEstado = json.idPiezaEstado;
+            return NuevoEstadiPieza;
+
+        }
+    }
+
+ }
+
+ 
+ 
+  
 class Usuario {
     constructor(nombre, usuario, pass, token, perfil) {
         this.nombre = nombre;
@@ -54,7 +210,7 @@ class Usuario {
         }
     }
 }
-
+/*
 class Perfil {
     constructor() {
         this.cuso = [];
@@ -67,6 +223,6 @@ class Perfil {
     delCasoUso(cu) {
         this.cuso = this.cuso.filter(x => x !== cu);
     }
-}
+}*/
 
-module.exports = { Pieza, Usuario, Perfil };
+module.exports = { Pieza, Prestamo, Taxidermia , EstadiPieza, Piezaestado, Prestamoestado};
